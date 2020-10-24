8fef51dcd6f1c047ed87dd28475901a804aa1d55
Marcus Chá <70450187+marcus-cha@users.noreply.github.com>
2020-10-23 21:05:36 -0300

    Adicionando arquivos

---
+++ javascript.js
@@ -0,0 +1,6 @@
+let anoAtual = '2020';
+let nome, idade, texto;
+nome = prompt('Digite seu nome: ')
+let anoNascimento = prompt('Digite o ano em que você nasceu: ')
+idade = anoAtual-anoNascimento;
+texto = alert(`Bem-Vindo, seu nome é ${nome} e você tem ${idade} anos`);
